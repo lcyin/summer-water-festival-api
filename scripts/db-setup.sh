ff27192ddb95235907ba2f8c3119fcb05405e843
lcyin <laichunyinkevin@gmailcom>
2025-04-28 23:30:28 +0800

    move db setup script path

--- scripts/db-setup.sh
+++ scripts/db-setup.sh
@@ -9,7 +9,7 @@ PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -p $DB_PORT -U $DB_USER -d postgres -c 
 
 # Run migrations
 echo "Running migrations..."
-for file in $(ls sql/*.sql | sort); do
+for file in $(ls src/db/migrations/*.sql | sort); do
     echo "Running $file..."
     PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -p $DB_PORT -d $DB_NAME -U $DB_USER -f $file
 done 
